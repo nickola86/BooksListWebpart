70cdea8560e77cf425ccd56a64354fa5ea17526b
Nicola Di Trani <n.di.trani@avanade.com>
2023-06-07 09:40:40 +0200

    anno e numero di pagine validation

--- src/webparts/booksList/components/BookModal.tsx
+++ src/webparts/booksList/components/BookModal.tsx
@@ -33,6 +33,8 @@ export const BookModal = (props:IBookProps): JSX.Element => {
 
   const _book: IBook = {titolo,autoreLibro,pagineLibro,annoPubblicazione,id,asString:`${titolo} - ${autoreLibro} (pagine: ${pagineLibro}, anno:${annoPubblicazione})` || asString}
 
+  const greaterThanZeroOrEmpty = (v:number | string):boolean => {return v==="" || parseInt(v as string)>0}
+
   return (<Modal
     isOpen={props.isModalOpen}
     onDismiss={()=>{props.onCloseCallback(book,false)}}
@@ -42,8 +44,8 @@ export const BookModal = (props:IBookProps): JSX.Element => {
       <h1>{props.isEditMode ? strings.editBook : strings.newBook}</h1>
       <TextField label={strings.name} value={titolo} onChange={(e,v)=>setTitolo(v)}/>
       <TextField label={strings.autoreLibro}value={autoreLibro} onChange={(e,v)=>setAutoreLibro(v)}/>
-      <TextField label={strings.pagineLibro} value={pagineLibro} onChange={(e,v)=>setPagineLibro(v)}/>
-      <TextField label={strings.annoPubblicazione} value={annoPubblicazione} onChange={(e,v)=>setAnnoPubblicazione(v)}/>
+      <TextField label={strings.pagineLibro} type='number' value={pagineLibro} onChange={(e,v)=>greaterThanZeroOrEmpty(v) && setPagineLibro(v)}/>
+      <TextField label={strings.annoPubblicazione} type='number' value={annoPubblicazione} onChange={(e,v)=>greaterThanZeroOrEmpty(v) && setAnnoPubblicazione(v)}/>
       <div className={styles.buttons}>
         <DefaultButton onClick={()=>{props.onCloseCallback(_book,false)}}>{strings.close}</DefaultButton>
         <DefaultButton primary onClick={()=>{props.onCloseCallback(_book,true)}}>{strings.save}</DefaultButton>
