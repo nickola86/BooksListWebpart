59e080721d53c4b83156cc10a98780a286874a2c
Nicola Di Trani <n.di.trani@avanade.com>
2023-06-09 17:11:35 +0200

    release 1.0.0.2

--- src/webparts/booksList/components/User.tsx
+++ src/webparts/booksList/components/User.tsx
@@ -7,11 +7,15 @@ export const User = (): JSX.Element => {
     const [currentUser,setCurrentUser] = useState<IUser>({} as IUser)
 
     useEffect(()=>{
-        userService.me().then((user:IUser)=>{
-            console.log("User from graph:", user)
-            setCurrentUser(user)
-        })
+        myHandler()
     },[])
+
+    const myHandler = async () => {
+        const user:IUser = await userService.me()
+        console.log("User from graph:", user)
+        setCurrentUser(user)
+    }
+
     return <div>
         {!!currentUser && <IdentityCard user={currentUser}  />}
         {!currentUser && <p>Caricamento dati utente da graph non riuscito</p>}
